c2ff6e8c8d56a1b7acb452327d37db0f41ba8ed8
JSDev1999 <rameshlearningadvance@gmail.com>
2022-12-31 10:11:23 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Fragment>
       <Container>
-        <Typography>hello there</Typography>
+        <Typography>hello there funy guy</Typography>
       </Container>
     </Fragment>
   );
